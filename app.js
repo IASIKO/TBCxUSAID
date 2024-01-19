d750f5768e1321a63fc3beccf8e1968a044c294c
IASIKO <iaseshviligi@gmail.com>
2024-01-19 20:50:58 +0400

    save header opacity

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+// header
+
+document.addEventListener("DOMContentLoaded", function () {
+  let header = document.querySelector(".headerWrapper");
+  let headerHeight = header.offsetHeight;
+
+  window.addEventListener("scroll", function () {
+    let scrollTop = window.scrollY;
+
+    if (scrollTop > headerHeight) {
+      header.classList.add("headerTransparent");
+    } else {
+      header.classList.remove("headerTransparent");
+    }
+  });
+});
+
 // accordion
 
 let acc = document.querySelectorAll(".questionBox");
