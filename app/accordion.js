38d51c24b6c3ed14b579a364f46ef972ca1dff3b
IASIKO <iaseshviligi@gmail.com>
2024-01-21 14:51:54 +0400

    complete accordion

--- app/accordion.js
+++ app/accordion.js
@@ -6,7 +6,7 @@ for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     for (let j = 0; j < answerTexts.length; j++) {
       if (i !== j) {
-        answerTexts[j].style.display = "none";
+        answerTexts[j].style.height = "0";
         arrow[j].classList.remove("active");
       }
     }
@@ -14,10 +14,10 @@ for (let i = 0; i < acc.length; i++) {
     arrow[i].classList.toggle("active");
 
     let answerText = this.nextElementSibling;
-    if (answerText.style.display === "block") {
-      answerText.style.display = "none";
+    if (answerText.style.height === "0px" || answerText.style.height === "") {
+      answerText.style.height = answerText.scrollHeight + "px";
     } else {
-      answerText.style.display = "block";
+      answerText.style.height = "0";
     }
   });
 }
