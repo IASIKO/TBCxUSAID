2d154096ab5d54e5ce3343bfefbc3e42acf3c2bf
IASIKO <iaseshviligi@gmail.com>
2024-01-23 13:17:14 +0400

    update changes

--- app/header/header.js
+++ app/header/header.js
@@ -29,4 +29,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
     body.classList.remove("disable-scroll");
   });
+
+  function createNavList() {
+    const screenWidth = window.innerWidth;
+
+    const navContainer = document.querySelector(".navList");
+    const sidebarContainer = document.querySelector(".sidebarNavList");
+
+    if (navContainer && sidebarContainer) {
+      navContainer.innerHTML = "";
+      sidebarContainer.innerHTML = "";
+
+      screenWidth > 600
+        ? createListItems(navContainer, "pageName")
+        : createListItems(sidebarContainer, "sidebarPageName");
+    }
+  }
+
+  function createListItems(container, className) {
+    const pages = ["მთავარი", "TBC IT", "TBC x USAID", "რისკები"];
+
+    pages.forEach((page) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+
+      a.href = "#";
+      a.textContent = page;
+      a.classList.add(className);
+
+      page === "TBC x USAID" && a.classList.add("activePage");
+
+      li.appendChild(a);
+      container.appendChild(li);
+    });
+  }
+
+  createNavList();
+
+  window.addEventListener("resize", createNavList);
 });
